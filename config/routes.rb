1d8824ca32253d80b414d24ff26e48abbad2b990
YOUR NAME <YOUREM@IL.COM>
2015-01-09 10:30:49 -0500

    added basic controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root "jokes#index"
+
+  resources :users
+  resources :jokes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
