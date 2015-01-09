fd89252fc337658766c271200138ed9b6d4daf92
Rebecca Mills <vena.cava@gmail.com>
2015-01-09 13:02:52 -0500

    fixing downvote issue

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,20 @@
 class VotesController < ApplicationController
-  
+
+  before_action :authenticate_user!
+
+  def upvote
+  	joke = Joke.find(params[:joke_id])
+  	vote = joke.votes.new({rating: true, user_id: current_user.id})
+  end
+
+  def downvote
+  	joke = Joke.find(params[:joke_id]) 
+  	vote = Vote.new({rating: false, user_id: 3, joke_id: joke.id})
+  	if vote.save 
+  		redirect_to "/jokes"
+  	end
+  end
 end
+
+
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
 	belongs_to :joke
 	belongs_to :user
-	validates_presence_of :rating
+	# validates_presence_of :rating
 end
