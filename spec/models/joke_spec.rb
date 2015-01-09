0be8d3a5714db55415e1c42f77d457dcfddf5971
Rebecca Mills <vena.cava@gmail.com>
2015-01-09 10:45:35 -0500

    added validation test to spec

---
+++ spec/models/joke_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Joke do
+	describe "::new" do	
+		it "can be instantiated" do
+			joke = Joke.new
+			expect(joke.class).to eq(Joke)
+		end
+	end
+	it { should have_many(:votes)}
+	it { should belong_to(:joke)}
+	it { should validate_presence_of(:body)}
+end
