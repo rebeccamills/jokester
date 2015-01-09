6ecd813f75ec35b9c1983e89b90641bbcd595ed0
Rebecca Mills <vena.cava@gmail.com>
2015-01-09 15:56:50 -0500

    crud functionality

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,18 +1,58 @@
 class JokesController < ApplicationController
 	before_action :authenticate_user!
+	before_action :set_joke, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
   def index
   	@jokes = Joke.all
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  	@joke = Joke.new
+  end
+
+  def create
+  	@joke = current_user.jokes.build(joke_params)
+  	if @joke.save
+  		redirect_to joke_path(@joke)
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  	@joke.update(joke_params)
+  	if @joke.save
+  		redirect_to joke_path(@joke)
+  	else
+  		render :edit
+  	end
+  end
+  def destroy
+  	@joke.destroy
+  	redirect_to jokes_path
+  end
+
   def upvote
-  	@joke = Joke.find(params[:id])
   	@joke.upvote_by current_user
   	redirect_to jokes_path
   end
 
   def downvote
-  	@joke = Joke.find(params[:id])
   	@joke.downvote_by current_user
   	redirect_to jokes_path
+  end
+
+  private
+  def joke_params
+  	params.require(:joke).permit(:body)
+  end
+
+  def set_joke
+  	@joke = Joke.find(params[:id])
   end
 end
