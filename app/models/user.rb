b3350b315d1022db461ff938c24f290871265141
Rebecca Mills <vena.cava@gmail.com>
2015-01-09 11:08:35 -0500

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	has_many :votes
+	has_many :votes, :through => :jokes
 	has_many :jokes
+	has_many :comments, :through => :jokes
 	has_many :friends, :through => :friendships
 	has_many :friendships
 	validates :email, :encrypted_password, :sign_in_count, presence: true
